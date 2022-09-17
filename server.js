dafa3d01b335f701679e61fb13e1ed0d8763d26e
rsnr-code <rishadnero@gmail.com>
2022-09-17 15:36:56 -0400

    reorganized/cleaned up server.js file

--- server.js
+++ server.js
@@ -1,62 +1,54 @@
 const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const connectDB = require('./config/db');
-const logger = require('morgan');
-const flash = require('express-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
-const MongoStore = require('connect-mongo');
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const connectDB = require("./config/db");
+const logger = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const MongoStore = require("connect-mongo");
 const mainRoutes = require("./routes/main");
 const noteRoutes = require("./routes/notes");
 
-
 require("dotenv").config({ path: "./config/.env" });
 
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 connectDB();
-app.use(logger('dev'))
 
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(logger("dev"));
 
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI,
-      }),
-    })
-  )
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
 
-  app.use(
-    methodOverride(function (req, res) {
-      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        let method = req.body._method
-        delete req.body._method
-        return method
-      }
-    })
-  )
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(flash())
-
-app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-  });
+app.use(flash());
 
 app.use("/", mainRoutes);
 app.use("/notes", noteRoutes);
